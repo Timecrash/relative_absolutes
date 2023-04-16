ea71b81c19a3efa01e21bc461a73c82729e586de
Timecrash <Timecrash@users.noreply.github.com>
2023-04-16 13:46:12 +0000

    jekyll build from Action 4e31c4d02322fe43fff7acefa3adf74d16bc4e1b

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,127 @@
+var store = [{
+        "title": "Introductions",
+        "excerpt":"Okay, here we go. At the insistence of my friend Karen, and the subtle prodding of my other friend Becca, I've finally gone about and made a professional writing website. Granted, I've got no idea what I'm doing here, and this place is likely to change wildly as the days...","categories": ["General"],
+        "tags": ["general","personal"],
+        "url": "/general/introductions",
+        "teaser": null
+      },{
+        "title": "Description and Dialogue",
+        "excerpt":"I don't have an easy time describing myself and my work. I remember a few years ago, when I first came up with a sci-fi setting I wanted to write a book in. This was hardly some massive worldbuilding project, just a confined world to set the book in, a...","categories": ["Writing"],
+        "tags": ["Airless","musing","Salazarre","stories","writing"],
+        "url": "/writing/description-and-dialogue",
+        "teaser": null
+      },{
+        "title": "Mall Fly",
+        "excerpt":"I'm currently sitting in the Willow Grove Mall, it's 8:30 AM, and I'm waiting for my car to be inspected. Apparently it might take anywhere from a couple hours to all day. All I brought to occupy me is my phone and my Kindle. So let's talk about malls. I'm...","categories": ["General"],
+        "tags": ["general","personal"],
+        "url": "/general/mall-fly",
+        "teaser": null
+      },{
+        "title": "Exploring Star Wars: Jedi Academy",
+        "excerpt":"I spent my teenage years with various video games, probably far too many to consider myself a writer. Games instead of stories, play mechanics instead of narratives. I learned how to fly a starfighter before I knew the proper use of dialogue tags. But this kind of background has led...","categories": ["Gaming"],
+        "tags": ["gaming","musing","star wars","writing"],
+        "url": "/gaming/exploring-star-wars-jedi-academy",
+        "teaser": null
+      },{
+        "title": "Building Salazarre, pt. I",
+        "excerpt":"Worldbuilding! The bane, or blessing, of any genre author. Countless articles have been written about it, how to do it, what to look for. I'm not out to make an assertive guide on it or anything. Mainly, I want this to touch upon how I'm handling it for my dark...","categories": ["Writing"],
+        "tags": ["Salazarre","writing"],
+        "url": "/writing/building-salazarre-pt-i",
+        "teaser": null
+      },{
+        "title": "Construct vs. Character",
+        "excerpt":"I seem to have found two broad ways to think about the people in stories. Of course, you can cut up that great literary monolith called 'CHARACTER' any way you want, depending on your views, experiences, or your mood at the time. This is merely one way that I do,...","categories": ["Writing"],
+        "tags": ["anime","characters","stories","writing"],
+        "url": "/writing/construct-vs-character",
+        "teaser": null
+      },{
+        "title": "Building Salazarre, pt. II",
+        "excerpt":"This is part two of a series of posts that detail how I'm handling worldbuilding for the first time. The dark fantasy setting of Salazarre is a lot of things, and it's gonna take me a while to discuss them all. I've come up with good chunk of it already,...","categories": ["Writing"],
+        "tags": ["Salazarre","writing"],
+        "url": "/writing/building-salazarre-pt-2",
+        "teaser": null
+      },{
+        "title": "Building Salazarre, pt. III",
+        "excerpt":"Here's part three of a series with no end in sight. Normally, I'd have this every other week, but I actually split part two in half, as there was more I wanted to talk about regarding fantasy names. So, technically this is part 2.5, but by George, I'm getting as...","categories": ["Writing"],
+        "tags": ["Salazarre","stories","writing"],
+        "url": "/writing/building-salazarre-pt-3",
+        "teaser": null
+      },{
+        "title": "Dreaming of Delis",
+        "excerpt":"(Header image via morgueFile: http://mrg.bz/LSsMGB) I have a strange relationship with dreams. I know the ones I have are quite vivid. Expansive, even beautiful settings, places I have never seen before, places I thought I knew quite well, with many little differences. I can tell that my imagination can concoct...","categories": ["Writing"],
+        "tags": ["dreams","writing"],
+        "url": "/writing/dreaming-of-delis",
+        "teaser": null
+      },{
+        "title": "Building Salazarre, pt. IV",
+        "excerpt":"Merry Christmas for those who celebrate it! I have no idea if I'll have something better to write for Christmas, as I'm making a bunch of these posts on December 1, so this might seem a little strange for a Christmas post. The image this time is from a trip...","categories": ["Writing"],
+        "tags": ["Salazarre","writing"],
+        "url": "/writing/building-salazarre-pt-iv",
+        "teaser": null
+      },{
+        "title": "The Woman Who Would Be King: A Review",
+        "excerpt":"Book reviews are something writers do, right? I'd like to review the books I'm reading after I'm done with them, it's a pretty decent way to get blog posts. Ideally I'd be tying it into how it inspired me in my own work, though I feel that could get narcissistic...","categories": ["Reviews"],
+        "tags": ["nonfiction","reviews","writing"],
+        "url": "/reviews/the-woman-who-would-be-king-a-review",
+        "teaser": null
+      },{
+        "title": "Dreaming of Houses",
+        "excerpt":"So I'm at a loss for a blog post this coming weekend. It's a Thursday as I write this, so I'm coming up against the clock a bit. I've got another book review scheduled for next week, but I'd rather not do two of those in a row. I figured...","categories": ["General"],
+        "tags": ["dreams","general","musing"],
+        "url": "/general/dreaming-of-houses",
+        "teaser": null
+      },{
+        "title": "The Way of Kings: A Review",
+        "excerpt":"I've never been one to write long reviews. I tend to lose track of what I want to say. I've got a little checklist of ideas I want to write about at the bottom of this post, erasing them once I've dealt with them. I'm a scatterbrain, and there's much...","categories": ["Reviews"],
+        "tags": ["fantasy","reviews","writing"],
+        "url": "/reviews/the-way-of-kings-a-review",
+        "teaser": null
+      },{
+        "title": "Building Salazarre, pt. V",
+        "excerpt":"You thought I forgot about this, didn't you? Jeeze, about a month has passed since I last did this, hasn't it? And here I am again frantically writing this post on a Thursday, two days before it's supposed to go live. I'm definitely interested in writing more about this, but...","categories": ["Writing"],
+        "tags": ["Salazarre","stories","writing"],
+        "url": "/writing/building-salazarre-pt-v",
+        "teaser": null
+      },{
+        "title": "Testing the Lights",
+        "excerpt":"Header image via Morguefile. Boy, you get this plan of making weekly blog posts, write down some good post ideas, and then four months pass by without so much as a 'brb.' Thankfully, I don't have much of an audience to disappoint yet. So yeah, I lost track of time....","categories": ["General"],
+        "tags": ["general","musing","personal","programming"],
+        "url": "/general/testing-the-lights",
+        "teaser": null
+      },{
+        "title": "NaNoWriMo 2016 - Pre-Planning",
+        "excerpt":"Header image via Morguefile. So here we are again. May to September. No point in dwelling on that, I suppose, though I did manage to finish the draft of The Witch and the Snake...back in July. Really should have made a post about that. Yay? Like I said, I'm not...","categories": ["Writing"],
+        "tags": ["nanowrimo","Starline","stories","writing"],
+        "url": "/writing/nanowrimo-2016-pre-planning",
+        "teaser": null
+      },{
+        "title": "Staying Motivated",
+        "excerpt":"A friend just asked on Facebook what tips I might have for staying motivated while researching/writing. Instead of making a few concise bullet-points, I figure I'll just blow it out into a whole blog post since, well, I do need more posts on this blog. Not to mention I'm not...","categories": ["Writing"],
+        "tags": ["musing","stories","writing"],
+        "url": "/writing/staying-motivated",
+        "teaser": null
+      },{
+        "title": "Thoughts on Popful Mail",
+        "excerpt":"So in the interests of writing something here, I thought I'd write a bit on a game I played recently, with a really silly title. Popful Mail was released on the Sega CD, which was itself an add-on to the Sega Genesis, back when add-ons to video game consoles were...","categories": ["Gaming"],
+        "tags": ["gaming","musing","personal"],
+        "url": "/gaming/thoughts-on-popful-mail",
+        "teaser": null
+      },{
+        "title": "Destroyer: A Review",
+        "excerpt":"I discovered Chris Fox through Instagram. I'm pretty sure it was Instagram. I found an ad for an eBook titled '5,000 Words Per Hour.' This is, of course, kind of a ridiculous goal, but I started reading and it put forward a decently well-explained method to do what the title...","categories": ["Reviews"],
+        "tags": ["reviews","sci-fi","writing"],
+        "url": "/reviews/destroyer-a-review",
+        "teaser": null
+      },{
+        "title": "Thoughts on Link's Awakening",
+        "excerpt":"I beat Link's Awakening for the first time since I was a kid, via the lovely Switch port. It's consistently been one of my favorite Zelda games, with only Ocarina of Time and maybe Breath of the Wild edging it out. Suffice to say, I have a lot of feelings...","categories": ["Gaming"],
+        "tags": ["gaming","zelda"],
+        "url": "/gaming/thoughts-on-links-awakening",
+        "teaser": null
+      },{
+        "title": "An Update",
+        "excerpt":"A view from the nature preserve near me, taken by my then-new Nikon D5600. Hi. You’ll notice the site looks a bit different, if you’ve ever read it prior to today (September 1, 2022). Back in 2020 or so, I decided to move my WordPress blog off of a hosted...","categories": ["General"],
+        "tags": ["general","personal"],
+        "url": "/general/an-update",
+        "teaser": null
+      }]
